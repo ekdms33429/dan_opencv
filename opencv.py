23c5266c8702d7142a5434f2fb79aeb40de7c5ad
dan.kim <dan.kim@kiwiplus.io>
2021-07-29 17:04:56 +0900

    commit

--- opencv.py
+++ opencv.py
@@ -16,18 +16,25 @@ class MyTestCase(unittest.TestCase):
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@text, '로그인')]")))
         driver.save_screenshot('screencap.png')
         '''
-        src = cv2.imread("screencap.png", cv2.IMREAD_GRAYSCALE)
-        templit = cv2.imread("img/kakaobtn.png", cv2.IMREAD_GRAYSCALE)
-        dst = cv2.imread("screencap.png")
+        src = cv2.imread("screencap.png", cv2.IMREAD_GRAYSCALE) #원본 이미지
+        templit = cv2.imread("img/kakaobtn.png", cv2.IMREAD_GRAYSCALE) # 템플릿 이미지, grayscale 이미지 사용
+        dst = cv2.imread("screencap.png") #결과이미지
 
-        result = cv2.matchTemplate(src, templit, cv2.TM_SQDIFF_NORMED)
+        result = cv2.matchTemplate(src, templit, cv2.TM_SQDIFF_NORMED) #템플릿 매칭 함수 matchTemplate(원본 이미지, 템플릿 이미지, 템플릿 매칭 플래그) / 이미지는 8비트의 단일 채널 이미지 사용
+        #매칭 수식 TM_SQDIFF / TM_SQDIFF_NORMED / TM_CCORR / TM_CCORR_NORMED / TM_CCOEFF / TM_CCOEFF_NORMED
+        #(W, H) 원본 이미지 크기 / (w, h) 템플릿 이미지 크기
+        # 결과값은 32비트 단일 채널, (W-w+1, H-h+1) 배열의 크기
+        # 템플릿 이미지로 비교하기 때문에 검출된 이미지 또한 템플릿 이미지와 동일한 크기
 
-        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
-        x, y = minLoc
+        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result) #최소, 최대 위치 함수 = 최소 포인터, 최대 포인터, 최소 지점, 최대 지점
+        x, y = minLoc # 검출 위치의 좌측 상단 모서리 좌표 = minLoc or maxLoc
         h, w = templit.shape
 
         dst = cv2.rectangle(dst, (x, y), (x + w, y + h), (0, 0, 255), 1)
-        cv2.imshow("dst", dst)
+        dst_scale = cv2.resize(dst, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_AREA) #fx, fx = 이미지 가로/세로 사이즈의 배수
+        #보간법 interpolation method: INTER_CUBIC 바이큐빅 보간법, 사이즈 늘릴 때(느림) / INTER_LINEAR 쌍선형 보간법, 사이즈 늘릴 때(default) / INTER_AREA 영역 보간법, 사이즈 줄일 때
+        #cv2.namedWindow("dst", cv2.WINDOW_NORMAL)
+        cv2.imshow("dst", dst_scale)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
