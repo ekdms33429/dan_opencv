0f199d13361fede89ef20609e7a439fc69e95f08
dan.kim <dan.kim@kiwiplus.io>
2021-08-02 12:37:02 +0900

    commit

--- tmpmatching.py
+++ tmpmatching.py
@@ -1,3 +1,4 @@
+#https://m.blog.naver.com/samsjang/220587092117
 import cv2
 import numpy as np
 from appium.webdriver.common.touch_action import TouchAction
@@ -7,17 +8,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
 def tmpMatching(thr):
     driver = android_driver()
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@text, '로그인')]")))
     driver.save_screenshot('screencap.png')
 
-    img = cv2.imread('screencap.png')
-    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread('img/kakaobtn.png', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('screencap.png') #원본 이미지
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #원본 이미지
+    template = cv2.imread('img/kakaobtn.png', cv2.IMREAD_GRAYSCALE) # 템플릿 이미지, grayscale 이미지 사용
     w, h = template.shape[::-1]
 
-    res = cv2.matchTemplate(imgray, template, cv2.TM_CCOEFF_NORMED)
+    res = cv2.matchTemplate(imgray, template, cv2.TM_CCOEFF_NORMED)#템플릿 매칭 함수 matchTemplate(원본 이미지, 템플릿 이미지, 템플릿 매칭 플래그) / 이미지는 8비트의 단일 채널 이미지 사용
+        #매칭 수식 TM_SQDIFF / TM_SQDIFF_NORMED / TM_CCORR / TM_CCORR_NORMED / TM_CCOEFF / TM_CCOEFF_NORMED
+        #(W, H) 원본 이미지 크기 / (w, h) 템플릿 이미지 크기
+        # 결과값은 32비트 단일 채널, (W-w+1, H-h+1) 배열의 크기
+        # 템플릿 이미지로 비교하기 때문에 검출된 이미지 또한 템플릿 이미지와 동일한 크기
     loc = np.where(res >= thr) # (array_row, array_column) row = y, column = x
 
     for pt in zip(*loc[::-1]):
